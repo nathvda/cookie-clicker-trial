060e9558372b2210f1c7cd443409039f8c1fa98d
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2023-07-15 18:20:36 +0200

    fix: reset button now reload page

--- modules/resetGame.js
+++ modules/resetGame.js
@@ -1,24 +1,24 @@
-import { saveGame } from "./saveGame.js";
-import { displayAchievements } from "./displayAchivements.js";
-import { displayValueList } from "./displayValueList.js";
-import { createUpgrades } from "./createUpgrades.js";
-import { displayScore } from "./displayScore.js";
 import { Player } from "./classes/Player.js";
+import { createUpgrades } from "./createUpgrades.js";
+import { displayAchievements } from "./displayAchivements.js";
+import { displayScore } from "./displayScore.js";
+import { displayValueList } from "./displayValueList.js";
 import { loadGame } from "./loadGame.js";
+import { saveGame } from "./saveGame.js";
 
 export function resetGame(joueur) {
-    let reset = confirm("Are you sure you want to reset?");
-    if (reset == true) {
-      joueur = Player
-  
-      saveGame(joueur);
-      loadGame(joueur);
-      displayScore(joueur);
-      displayAchievements(joueur);
-      displayValueList(joueur);
-      createUpgrades(joueur);
+  let reset = confirm("Are you sure you want to reset?");
+  if (reset == true) {
+    joueur = Player;
 
-    } else {
-      console.log("Reset annulé");
-    }
+    saveGame(joueur);
+    loadGame(joueur);
+    displayScore(joueur);
+    displayAchievements(joueur);
+    displayValueList(joueur);
+    createUpgrades(joueur);
+    window.location.reload();
+  } else {
+    console.log("Reset annulé");
   }
+}
